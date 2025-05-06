fdbc3eca68e128dabf6483a6ed007331fddf9f30
Sandstorm831 <gargrohan138@gmail.com>
2025-05-06 22:19:02 +0530

    completed the querying system, made tables and input systems

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PGliteWorker } from "@electric-sql/pglite/worker";
 import { live, type LiveNamespace } from "@electric-sql/pglite/live";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Cardio } from "ldrs/react";
+import "ldrs/react/Cardio.css";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const global_color_code = "rgb(135, 38, 87)";
+const global_color_code_hex = "#872657";
 
 type rowObject = {
   id: number;
   name: string;
+  age: number;
+  gender: string;
 };
 
 function isRowObjectType(item: { [key: string]: unknown }): item is rowObject {
@@ -19,19 +34,48 @@ function isRowObjectType(item: { [key: string]: unknown }): item is rowObject {
 
 type dbworker = (PGliteWorker & { live: LiveNamespace }) | null;
 let db: dbworker = null;
+let timeout: NodeJS.Timeout | null = null;
 
 function App() {
-  // let items: { [key: string]: any }[] = [];
+  const [loading, setLoading] = useState<boolean>(true);
   const [items, setItems] = useState<{ [key: string]: any }[]>([]);
+  const [query, setQuery] = useState<string>("");
+  const [qres, setQres] = useState<{ [key: string]: any }[]>([]);
   async function insertor(formData: FormData) {
     const name = formData.get("name");
+    const age = formData.get("age");
+    const gender = formData.get("gender");
+    console.log(name, age, gender);
     if (db) {
-      await db.query("INSERT INTO people (name) VALUES ($1)", [name]);
+      await db.query(
+        "INSERT INTO patient (name, age, gender) VALUES ($1, $2, $3)",
+        [name, age, gender]
+      );
       console.log(`${name} is inserted into the db`);
     } else {
       console.log("db is undefined");
       console.log("printing the undefined items rows");
       console.log(items);
+    }
+  }
+
+  async function runQuery() {
+    if (query.length == 0) {
+      setQres([]);
+      return;
+    }
+    if (db) {
+      const dbquery = `SELECT * 
+      FROM patient
+      WHERE name ILIKE '%${query}%'
+      LIMIT 10;`;
+      console.log(dbquery);
+      const res = await db.query<{ [key: string]: any }>(dbquery);
+      console.log("printing result array");
+      console.log(res);
+      setQres(res.rows);
+    } else {
+      console.log("db is not defined");
     }
   }
 
@@ -48,14 +92,16 @@ function App() {
       console.log("Creating table...");
 
       await pg.exec(`
-      CREATE TABLE IF NOT EXISTS people (
+      CREATE TABLE IF NOT EXISTS patient (
           id SERIAL PRIMARY KEY,
-          name TEXT NOT NULL
+          name TEXT NOT NULL,
+          age INTEGER NOT NULL,
+          gender VARCHAR(10) NOT NULL
       );
       `);
       db = pg;
       console.log("table creation done");
-      await pg.live.query(`SELECT * FROM people`, [], (res) => {
+      await pg.live.query(`SELECT * FROM patient`, [], (res) => {
         console.log("printing result");
         console.log(res.rows);
         const x = res.rows;
@@ -64,7 +110,7 @@ function App() {
         console.log(items);
       });
       console.log("are you coming here??");
-      db = pg;
+      setLoading(false);
     }
     /////////////////////////////////////////
 
@@ -82,16 +128,265 @@ function App() {
     creator();
   }, []);
 
-  return (
-    <>
-      <div className="w-full h-1/2 bg-gray-300">
-        <div className="text-gray-300">Hello</div>
-        <form action={insertor}>
-          <input name="name" />
-          <button type="submit">Insert</button>
-        </form>
+  useEffect(() => {
+    timeout = setTimeout(() => {
+      runQuery();
+    }, 1000);
+
+    return () => {
+      if (timeout !== null) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+    };
+  }, [query]);
+
+  return loading ? (
+    <div className="h-screen w-full flex flex-col justify-center">
+      <div className="w-full flex justify-center">
+        <Cardio size="100" stroke="4" speed="1.2" color={`rgb(135, 38, 87)`} />
       </div>
-      <div className="flex flex-col grow-1">
+    </div>
+  ) : (
+    <div className="h-full w-full p-5">
+      <Tabs defaultValue="register_patient" className="w-full">
+        <TabsList className="w-full h-16">
+          <TabsTrigger
+            value="register_patient"
+            className={`w-1/2 text-3xl text-[rgb(135, 38, 87)] `}
+          >
+            Register Patient
+          </TabsTrigger>
+          <TabsTrigger
+            value="query_patient"
+            className={`w-1/2 text-3xl text-[rgb(135, 38, 87)] `}
+          >
+            Find patient record
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="register_patient" className="w-full">
+          <Dialog>
+            <div className="w-full flex justify-center h-12 my-5">
+              <DialogTrigger
+                className={`w-2/5 flex flex-col justify-center text-3xl bg-[#872657] text-gray-200 rounded-lg shadow-sm hover:shadow-lg transition duration-200 hover:scale-101 cursor-pointer font-mono`}
+              >
+                <div>Input patient details</div>
+              </DialogTrigger>
+            </div>
+            <DialogContent className="w-full m-5">
+              <DialogHeader>
+                <DialogTitle className="w-full flex justify-center text-3xl text-[#872657]">
+                  Enter patient's data
+                </DialogTitle>
+                <DialogDescription>
+                  <form action={insertor}>
+                    {/* Input component */}
+                    <div className="relative h-14 w-full my-4 flex justify-center">
+                      <input
+                        className="peer text-[#444746] static outline-none bg-white w-full h-full border border-[#444746] rounded-md p-4 focus:border-[#0B57D0] transition duration-200 ease-out"
+                        type="text"
+                        placeholder=" "
+                        name="name"
+                        required
+                      />
+                      <label className="absolute bg-white text-[#444746] left-4 bottom-4 peer-focus:-translate-x-[5px] peer-focus:-translate-y-[31px] peer-focus:scale-95 peer peer-focus:text-[#0B57D0] transition duration-200 ease-out peer-[:not(:placeholder-shown)]:-translate-x-[5px] peer-[:not(:placeholder-shown)]:-translate-y-[31px] peer-[:not(:placeholder-shown)]:scale-95">
+                        Full Name
+                      </label>
+                    </div>
+                    {/* Upto here */}
+
+                    {/* My radio button component */}
+                    <div className="flex flex-col space-y-2 my-4 ms-1 w-full">
+                      <label className="text-gray-700 font-medium w-96">
+                        Gender
+                      </label>
+
+                      <div className="flex items-center space-x-6">
+                        <label className="inline-flex items-center cursor-pointer">
+                          <input
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            required
+                            className="peer hidden"
+                          />
+                          <div className="w-5 h-5 rounded-full border-2 border-gray-400 peer-checked:border-blue-600 peer-checked:bg-blue-600 flex items-center justify-center transition-colors">
+                            <div className="w-2.5 h-2.5 rounded-full bg-white peer-checked:bg-white" />
+                          </div>
+                          <span className="ml-2 text-gray-800">Male</span>
+                        </label>
+
+                        <label className="inline-flex items-center cursor-pointer">
+                          <input
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            className="peer hidden"
+                          />
+                          <div className="w-5 h-5 rounded-full border-2 border-gray-400 peer-checked:border-pink-500 peer-checked:bg-pink-500 flex items-center justify-center transition-colors">
+                            <div className="w-2.5 h-2.5 rounded-full bg-white peer-checked:bg-white" />
+                          </div>
+                          <span className="ml-2 text-gray-800">Female</span>
+                        </label>
+                      </div>
+                    </div>
+                    {/* upto here */}
+                    {/* Age iput component starts from here */}
+                    <div className="relative h-14 w-full my-4 ">
+                      <input
+                        className="peer text-[#444746] static outline-none bg-white w-full h-full border border-[#444746] rounded-md p-4 focus:border-[#0B57D0] transition duration-200 ease-out"
+                        type="number"
+                        placeholder=" "
+                        name="age"
+                        min={0}
+                        max={150}
+                        required
+                      />
+                      <label className="absolute bg-white text-[#444746] left-4 bottom-4 peer-focus:-translate-x-[5px] peer-focus:-translate-y-[31px] peer-focus:scale-95 peer peer-focus:text-[#0B57D0] transition duration-200 ease-out peer-[:not(:placeholder-shown)]:-translate-x-[5px] peer-[:not(:placeholder-shown)]:-translate-y-[31px] peer-[:not(:placeholder-shown)]:scale-95">
+                        Age
+                      </label>
+                    </div>
+                    {/* Upto here */}
+                    <button
+                      type="submit"
+                      className="h-14 w-full flex justify-center bg-[#872657] text-white text-2xl rounded-lg hover:shadow-lg transition duration-200 cursor-pointer"
+                    >
+                      <div className="h-full flex flex-col justify-center">
+                        Insert
+                      </div>
+                    </button>
+                    {/* <DialogClose asChild>
+                        <button type="button">Close</button>
+                      </DialogClose> */}
+                  </form>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+          <div className="flex justify-center w-full font-mono">
+            <div className="w-4/5 grid grid-cols-4 grid-auto-flow-row auto-rows-[50px] bg-[#872657] border rounded-lg">
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">
+                  PatientId
+                </div>
+              </div>
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">Name</div>
+              </div>
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">Age</div>
+              </div>
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">
+                  Gender
+                </div>
+              </div>
+              {items && items.length
+                ? items.map((item, id: number) => {
+                    if (isRowObjectType(item)) {
+                      return (
+                        <React.Fragment key={id}>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.id}
+                            </div>
+                          </div>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.name}
+                            </div>
+                          </div>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.age}
+                            </div>
+                          </div>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.gender}
+                            </div>
+                          </div>
+                        </React.Fragment>
+                      );
+                    } else {
+                      return null;
+                    }
+                  })
+                : null}
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="query_patient">
+          {/* Insert for querying logic */}
+          <div className="w-full flex justify-center mb-5">
+            <input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Enter the name of patient"
+              type="text"
+              className="h-16 w-4/5 px-8 py-2 text-2xl border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+          <div className="flex justify-center w-full font-mono">
+            <div className="w-4/5 grid grid-cols-4 grid-auto-flow-row auto-rows-[50px] bg-[#872657] border rounded-lg">
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">
+                  PatientId
+                </div>
+              </div>
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">Name</div>
+              </div>
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">Age</div>
+              </div>
+              <div className="border-[#872657] rounded-lg bg-[#872657] text-[#fffefc] text-3xl flex justify-center w-full">
+                <div className="flex flex-col h-full justify-center">
+                  Gender
+                </div>
+              </div>
+              {qres && qres.length
+                ? qres.map((item, id: number) => {
+                    if (isRowObjectType(item)) {
+                      return (
+                        <React.Fragment key={id}>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.id}
+                            </div>
+                          </div>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.name}
+                            </div>
+                          </div>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.age}
+                            </div>
+                          </div>
+                          <div className="px-2 h-full bg-white text-black text-xl flex justify-center w-full font-mono">
+                            <div className="flex flex-col justify-center">
+                              {item.gender}
+                            </div>
+                          </div>
+                        </React.Fragment>
+                      );
+                    } else {
+                      return null;
+                    }
+                  })
+                : null}
+            </div>
+          </div>
+        </TabsContent>
+      </Tabs>
+
+      {/*  */}
+
+      {/*  */}
+
+      {/* <div className="flex flex-col grow-1">
         {items && items.length
           ? items.map((item, id: number) => {
               if (isRowObjectType(item)) {
@@ -101,18 +396,8 @@ function App() {
               }
             })
           : null}
-      </div>
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          Make changes to your account here.
-        </TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
-      </Tabs>
-    </>
+      </div> */}
+    </div>
   );
 }
 
