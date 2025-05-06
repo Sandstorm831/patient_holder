1dd00c50c7b42d4fe3bd60b1c0b77f852022317a
Sandstorm831 <gargrohan138@gmail.com>
2025-05-06 10:35:07 +0530

    completed the pglite functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,50 @@
+import { usePGlite, useLiveQuery } from "@electric-sql/pglite-react";
+
+type rowObject = {
+  id: number;
+  name: string;
+};
+
+function isRowObjectType(item: { [key: string]: unknown }): item is rowObject {
+  return (
+    typeof item === "object" &&
+    item !== null &&
+    typeof item["id"] === "number" &&
+    typeof item["name"] === "string"
+  );
+}
+
 function App() {
+  const db = usePGlite();
+  const items = useLiveQuery(`SELECT * FROM people`);
+  console.log(typeof items);
+  console.log(items);
   function insertor(formData: FormData) {
     const name = formData.get("name");
-    pg.query("INSERT INTO people (name) VALUES ($1)", [name]);
+    db.query("INSERT INTO people (name) VALUES ($1)", [name]);
     console.log(`${name} is inserted into the db`);
   }
 
   return (
     <>
-      <div className="text-gray-300">Hello</div>
-      <form action={insertor}>
-        <input name="name" />
-        <button type="submit">Insert</button>
-      </form>
+      <div className="w-full h-1/2 bg-gray-300">
+        <div className="text-gray-300">Hello</div>
+        <form action={insertor}>
+          <input name="name" />
+          <button type="submit">Insert</button>
+        </form>
+      </div>
+      <div className="flex flex-col grow-1">
+        {items
+          ? items.rows.map((item, id: number) => {
+              if (isRowObjectType(item)) {
+                return <div key={id}>{item.name}</div>;
+              } else {
+                return null;
+              }
+            })
+          : null}
+      </div>
     </>
   );
 }
